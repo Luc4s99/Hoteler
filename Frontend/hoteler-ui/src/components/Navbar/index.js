50b0a2fd6264b33ab0937b0a9956504055fc51aa
LucasGalmeida <thelgda2@gmail.com>
2021-04-03 10:06:14 -0300

    Corrigido css da pagina home

--- Frontend/hoteler-ui/src/components/Navbar/index.js
+++ Frontend/hoteler-ui/src/components/Navbar/index.js
@@ -5,16 +5,17 @@ import './navbar.css';
 function Navbar (){
     return (
        <nav className="navbar navbar-expand-lg">
-                <span className="navbar-brand text-white font-weight-bold">Hoteler</span>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-                   aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                   <i className="fas fa-bars text-white"></i>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav mr-auto">
-                     <li className="nav-item">  <Link className="nav-link" to="/telaInicial">Voltar a tela inicial</Link>   </li>
-                  </ul>  
-                </div>
+         <span className="navbar-brand text-white font-weight-bold">Hoteler</span>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="fas fa-bars text-white"></i>
+         </button>
+         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+               <li className="nav-item">  <Link className="nav-link" to="/telaInicial">Voltar a tela inicial</Link>   </li>
+               <li className="nav-item ">  <Link className="nav-link" to="/">Sair</Link>   </li>
+            </ul>  
+         </div>
        </nav>
     )
 }
--- Frontend/hoteler-ui/src/view/home/index.js
+++ Frontend/hoteler-ui/src/view/home/index.js
@@ -9,17 +9,17 @@ function Home() {
     <>
       <div classname="homeContainer">
         
-        <div>
+        <div className="homeDiv">
 
           <img src={Logo} alt='Logo do site' />
 
         </div>
 
-        <div className="conteudo">
+        <div className="homeConteudo">
 
           <h1>Bem vindo ao Hoteler!</h1>
 
-          <h4>Uma aplicação fácil e intuitiva para gerenciamento do seu hotel!</h4>
+          <h4 className="homeH4">Uma aplicação fácil e intuitiva para gerenciamento do seu hotel!</h4>
 
           <Link to="/login" className="btn btn-success btn-lg">Login</Link>
 
